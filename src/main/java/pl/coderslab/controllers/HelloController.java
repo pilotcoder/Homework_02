894a85d51ab25c8457e1644cb2e2cc02e1149bba
pilotcoders <awings@o2.pl>
2023-05-09 19:27:33 +0200

    zadania domowe dzień 4 / zad 3 wczytanie pliku

--- src/main/java/pl/coderslab/controllers/HelloController.java
+++ src/main/java/pl/coderslab/controllers/HelloController.java
@@ -5,7 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalTime;
+import java.util.*;
 
 @Controller
 public class HelloController {
@@ -15,6 +20,7 @@ public class HelloController {
   @GetMapping("/hello")
   @ResponseBody
   public String hello(){
+    System.out.println("hello");
     return "Hello Kapusta";
   }
 
@@ -32,6 +38,29 @@ public class HelloController {
     }
 
     return "home";
+  }
+
+@GetMapping("/workers")
+  public String workersAction(Model model){
+ Random random = new Random();
+ int rdm = random.nextInt(30)+1;
+    Path path1 = Paths.get("/home/marcin/Pobrane/Homework_02/Workers.txt");
+  System.out.println(Files.exists(path1));
+    try {
+      for (String line : Files.readAllLines(path1)) {
+        if(line.contains(String.valueOf(rdm))){
+          model.addAttribute("worker", line);
+          System.out.println(line);
+        }
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+
+
+    return "workers";
+
   }
 
 
---
+++ src/main/webapp/views/workers.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: marcin
+  Date: 09.05.2023
+  Time: 18:03
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+Tu coś będzie...
+${worker}
+</body>
+</html>
